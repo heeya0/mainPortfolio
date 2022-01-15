f94d81122b8f29a365fd556cdfc94deb7144418d
heeya0 <nodes_@naver.com>
2022-01-15 14:19:01 +0900

    모바일 사이드 메뉴 글자 안 보이는 버그 픽스

--- js/main.js
+++ js/main.js
@@ -45,3 +45,13 @@ $(function(){
 });
 
 // 타이핑 테스트
+// particle.min.js hosted on GitHub (https://github.com/JulianLaval/canvas-particle-network)
+
+var canvasDiv = document.getElementById('particle-canvas');
+var options = {
+  particleColor: '#e37575',
+  interactive: true,
+  speed: 'slow',
+  density: 'medium'
+};
+var particleCanvas = new ParticleNetwork(canvasDiv, options);
