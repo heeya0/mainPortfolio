788a3e1851f739d90bb8a04180326dcfc9d83d60
heeya0 <nodes_@naver.com>
2022-01-15 14:34:49 +0900

    파티클 추가

--- js/main.js
+++ js/main.js
@@ -44,14 +44,7 @@ $(function(){
   });
 });
 
-// 타이핑 테스트
 // particle.min.js hosted on GitHub (https://github.com/JulianLaval/canvas-particle-network)
-
-var canvasDiv = document.getElementById('particle-canvas');
-var options = {
-  particleColor: '#e37575',
-  interactive: true,
-  speed: 'slow',
-  density: 'medium'
-};
-var particleCanvas = new ParticleNetwork(canvasDiv, options);
+let canvasDiv = document.getElementById('particle-canvas');
+let options = {particleColor: '#e37575', interactive: true, speed: 'slow', density: 'medium'};
+let particleCanvas = new ParticleNetwork(canvasDiv, options);
